fe7ee9b50eda2ee0b68f6141c5400648867839a0
oguri-amedo <mungcal.d.lewis@gmail.com>
2023-02-11 15:49:56 +0900

    rails g controller コマンドで作成されるファイルを制限

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_075203) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "created_at", precision: 6,null: false
-    t.datetime "updated_at", precision: 6,null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at",null: false
   end
 
 end
