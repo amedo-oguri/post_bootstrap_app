f8dc50245b34fe02ed321781b7fed12665cfa8d9
oguri-amedo <mungcal.d.lewis@gmail.com>
2023-02-16 00:03:38 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.order(id: :asc)
   end
@@ -17,9 +18,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
   end
 
   def destroy
